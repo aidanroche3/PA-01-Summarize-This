503237eb5c56c5fd246354389ce09aa86d07aa31
Aidan Roche <aidanroche937@gmail.com>
2023-05-13 23:54:58 -0400

    Implemented WriteFilesToPath with tests. Fixes #7

---
+++ src/main/java/cs3500/pa01/WriteFilesToPath.java
@@ -0,0 +1,32 @@
+package cs3500.pa01;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * Class for writing a String to a given file
+ */
+public class WriteFilesToPath {
+
+  /**
+   * Writes a given output to a specified path
+   * @param outputPath the path to write to
+   * @param output the content to write at the path
+   */
+  public void writeAtPath(Path outputPath, String output) throws IOException {
+    Path path = Path.of(outputPath.toString());
+    FileWriter fileWriter;
+
+    try {
+      fileWriter = new FileWriter(path.toString());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+    for (int i = 0; i < output.length(); i++) {
+      fileWriter.write(output.charAt(i));
+    }
+    fileWriter.close();
+  }
+}
---
+++ src/test/java/cs3500/pa01/WriteFilesToPathTest.java
@@ -0,0 +1,42 @@
+package cs3500.pa01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class for testing WriteFilesToPath and its associated methods
+ */
+class WriteFilesToPathTest {
+
+  /**
+   * Tests the writeFilesToPath method
+   */
+  @Test
+  public void testWriteFilesToPath() throws IOException {
+    File arrays = Path.of("notes-root/arrays.md").toFile();
+    File testOne = Path.of("notes-root/test.md").toFile();
+    File vectors = Path.of("notes-root/vectors.md").toFile();
+    File java = Path.of("notes-root/lecture notes/java.md").toFile();
+    ArrayList<File> files = new ArrayList<>(Arrays.asList(arrays, testOne, vectors, java));
+    CombineFiles combine = new CombineFiles(files);
+    String combined = combine.getCombinedFiles();
+    FormatFile formatFile = new FormatFile(combined);
+    String output = formatFile.summarizeContent();
+
+    Path sample = Path.of("outputDirectory/samplesummary.md");
+    Path testTwo = Path.of("outputDirectory/test.md");
+    Path fake = Path.of("fakeDirectory/nonexistent.md");
+    WriteFilesToPath filesToPath = new WriteFilesToPath();
+    filesToPath.writeAtPath(testTwo, output);
+    assertEquals(-1, Files.mismatch(sample, testTwo));
+    assertThrows(RuntimeException.class, () -> filesToPath.writeAtPath(fake, output));
+  }
+
+}
