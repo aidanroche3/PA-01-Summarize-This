22c5ab32dcf4c43d54fe7a0f6d59e2f4ed58e17f
Aidan Roche <aidanroche937@gmail.com>
2023-05-14 21:20:07 -0400

    Ammending LastModifiedComparator to build on GitHub

--- src/test/java/cs3500/pa01/LastModifiedComparatorTest.java
+++ src/test/java/cs3500/pa01/LastModifiedComparatorTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileTime;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -16,12 +17,17 @@ public class LastModifiedComparatorTest {
    */
   @Test
   public void testCompare() {
-    File one = Path.of("src/tests/resources/notes-root/arrays.md").toFile();
-    File two = Path.of("src/tests/resources/notes-root/vectors.md").toFile();
-    MarkDownFile mdOne = new MarkDownFile(one);
-    MarkDownFile mdTwo = new MarkDownFile(two);
+
+    File arrays = Path.of("src/tests/resources/notes-root/arrays.md").toFile();
+    MarkDownFile one = new MarkDownFile(arrays,
+        FileTime.fromMillis(1683850965878L),
+        FileTime.fromMillis(1683850988417L));
+    File vectors = Path.of("src/tests/resources/notes-root/vectors.md").toFile();
+    MarkDownFile two = new MarkDownFile(vectors,
+        FileTime.fromMillis(1683851000934L),
+        FileTime.fromMillis(1683865690271L));
     LastModifiedComparator lmc = new LastModifiedComparator();
-    assertEquals(-1, lmc.compare(mdOne, mdTwo));
-    assertEquals(1, lmc.compare(mdTwo, mdOne));
+    assertEquals(-1, lmc.compare(one, two));
+    assertEquals(1, lmc.compare(two, one));
   }
 }
